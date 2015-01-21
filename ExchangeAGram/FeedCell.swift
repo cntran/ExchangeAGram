f83cbe4708143e73d11551d841e5d75f88013635
Craig Tran <cntran@gmail.com>
2015-01-20 16:39:32 -0800

    commit changes

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Craig Tran on 1/17/15.
+//  Copyright (c) 2015 SDBX Studio. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
---
+++ ExchangeAGram/FilterCell.swift
@@ -0,0 +1,27 @@
+//
+//  FilterCell.swift
+//  ExchangeAGram
+//
+//  Created by Craig Tran on 1/20/15.
+//  Copyright (c) 2015 SDBX Studio. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+    var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        contentView.addSubview(imageView)
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
